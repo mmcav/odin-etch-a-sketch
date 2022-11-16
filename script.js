88dfa031097646fb8abf3e7e08b9e95a7e9687a4
Marcelo <19808040+mmcav@users.noreply.github.com>
2022-11-16 15:41:51 -0300

    Grid button now functional, colors are now rgb() Edited script.js, now the Change Grid button changes the grid. Edited style.css to have the colors on rgb() code. Lastly, I commented the flex-basis out of the css, and inserted it into the js, to make sure the changed grid(s) will fit the window width.

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-const size = 16 * 16;
-const container = document.querySelector('div.container');
-let child;
-for (let i = 0; i < size; i++) {
-    child = document.createElement('div');
-    child.setAttribute('id', `element${i}`);
-    child.setAttribute('class', 'box');
-    container.appendChild(child);
+function makeGrid(size = 16) {
+    const container = document.querySelector('div.container');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let child;
+    const childFlexBasis = 1/size * 100;
+    for (let i = 0; i < (size * size); i++) {
+        child = document.createElement('div');
+        child.setAttribute('id', `element${i}`);
+        child.setAttribute('class', 'box');
+        child.setAttribute('style', `flex-basis: ${childFlexBasis}%`);
+        container.appendChild(child);
+    }
 }
+
+makeGrid()
 
 const resetButton = document.querySelector('button#reset');
 resetButton.addEventListener('click', () => {
     const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => {
-        box.style.backgroundColor = 'white';
+        box.style.backgroundColor = 'rgb(255, 255, 255)';
     });
 });
 
 const gridButton = document.querySelector('button#changegrid');
 gridButton.addEventListener('click', () => {
-    alert('placeholder message');
+    let newSize;
+    while (true) {
+        newSize = prompt('Input the new grid row/column size.\nPlease use a number between 1 and 100:');
+        if (newSize === null) break;
+        newSize = parseInt(newSize);
+        if (!isNaN(newSize) && newSize <= 100 && newSize >= 1) {
+            makeGrid(newSize);
+            break;
+        }
+    }
 });
