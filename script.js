ca26d604916e7fa1973fdb7625bc46475724581f
Marcelo <19808040+mmcav@users.noreply.github.com>
2022-11-16 18:05:12 -0300

    Drawing on grid now possible, plus style changes Edited style.css to make some changes to the document style, chief among them the way the container width is handled. Edited script.js to finally add the basic logic to draw on the grid.

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ function makeGrid(size = 16) {
     const childFlexBasis = 1/size * 100;
     for (let i = 0; i < (size * size); i++) {
         child = document.createElement('div');
-        child.setAttribute('id', `element${i}`);
         child.setAttribute('class', 'box');
-        child.setAttribute('style', `flex-basis: ${childFlexBasis}%`);
+        child.setAttribute('style', `background-color: rgb(255, 255, 255); flex-basis: ${childFlexBasis}%;`);
         container.appendChild(child);
     }
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener('mouseover', gridPaint);
+    });
+}
+
+function gridPaint() {
+    /*const currentColor = parseInt(this.style.backgroundColor.split(', ')[1]);
+    if (currentColor < 26) {
+        this.style.backgroundColor = 'rgb(0, 0, 0)';
+    } else {
+        this.style.backgroundColor = `rgb(${currentColor - 26}, ${currentColor - 26}, ${currentColor - 26})`;
+    }*/
+    this.style.backgroundColor = 'rgb(0, 0, 0)';
 }
 
 makeGrid()
@@ -29,7 +42,7 @@ gridButton.addEventListener('click', () => {
     let newSize;
     while (true) {
         newSize = prompt('Input the new grid row/column size.\nPlease use a number between 1 and 100:');
-        if (newSize === null) break;
+        if (newSize === null) break; // Cancel button
         newSize = parseInt(newSize);
         if (!isNaN(newSize) && newSize <= 100 && newSize >= 1) {
             makeGrid(newSize);
